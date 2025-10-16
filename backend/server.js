c7655b99e4c2d96b8391f35ddb4fa1247e31f803
cientifico_datos_cor <35585360+Corlubu@users.noreply.github.com>
2025-10-16 12:51:50 -0400

    Update CORS origin to new frontend URL
    Frontend url

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(helmet());
 // Enable CORS (restrict to your frontend origin in production)
 const corsOptions = {
   ////origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  origin: process.env.FRONTEND_URL || 'https://cch-foodpantry.onrender.com/api',
+  origin: process.env.FRONTEND_URL || 'https://church-foodbank.vercel.app/',
   credentials: true,
   optionsSuccessStatus: 200
 };
