917e1b4e172d2f724cc4afc989a84f9867839612
cientifico_datos_cor <35585360+Corlubu@users.noreply.github.com>
2025-10-24 10:44:53 -0400

    Create .eslintrc.cjs

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+// frontend/.eslintrc.cjs
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime', // For new JSX transform
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off', // Optional: disable prop-types if using TypeScript or for simplicity
+  },
+};
